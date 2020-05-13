fa4e8469b58c257539b893bf5a78dee5bc04038b
Stephan Seitz <stephan.seitz@fau.de>
2020-05-13 19:01:28 +0200

    Do not require arrow

--- src/code_grader/lexer.py
+++ src/code_grader/lexer.py
@@ -22,7 +22,7 @@ class CustomJavaLexer(JavaLexer):
         'root': [
             (r'[^\S\n]+', Text),
             # Our magic comments
-            (r'///[\s]*<-.*?\n', Generic.Error),
+            (r'///[\s]*.*?\n', Generic.Error),
             (r'//[\s]*\[.*:.*(points)?\].*?\n', Generic.Error),
             # Copied from JavaLexer
             (r'//.*?\n', Comment.Single),
